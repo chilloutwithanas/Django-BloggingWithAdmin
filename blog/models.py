f38f0486927f31c264a0e0faf2a1ef71dd68b81d
Anas Khan <chilloutwithanas@gmail.com>
2021-03-16 00:07:00 +0530

    Building-Views-And-Adding-URLs

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+
+class PublishedManager(models.Manager):
+    '''customized manager to retrieve all posts with the published status'''
+    def get_queryset(self):
+        '''The get function of the customized manager'''
+        return super().get_queryset().filter(status='published')
 
 class Post (models.Model):
     '''This is Blog's Post Data Schema'''
@@ -20,7 +27,8 @@ class Post (models.Model):
                                on_delete=models.CASCADE,
                                related_name = 'blog_posts'
     )
-    
+    # testField = models.CharField(max_length=250, default='AnasKhanTesting')
+    body = models.TextField(default='Default Body')
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -34,3 +42,11 @@ class Post (models.Model):
         
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        '''To build the canonical URL for Post objects'''
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def post_list(request):
+    '''To (render) list all the published posts'''
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts':posts})
+    
+def post_detail(request, year, month, day, post):
+    '''To display a single post'''
+    post = get_object_or_404(Post,  slug=post,
+                                    status='published',
+                                    publish__year=year,
+                                    publish__month=month,
+                                    publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post':post}) 
+
+
