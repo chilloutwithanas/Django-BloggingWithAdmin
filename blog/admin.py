1e980364ed4d4a1c3ea672a469eb1b7d086d6e5c
Anas Khan <chilloutwithanas@gmail.com>
2021-03-15 12:39:00 +0530

    blog-with-model-admin-page

---
+++ blog/admin.py
@@ -0,0 +1,18 @@
+# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'satus')
+    list_filter = ('satus', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('satus',  'publish')
+
